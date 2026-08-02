f240f7c9f4c9f3eb2655f6631c71e247b1ec6f8a
Mr.Ashik Ali <mr.ashikali18@gmail.com>
2026-08-02 20:54:14 +0530

    Delete careers directory

--- careers/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CareersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "careers"
-    verbose_name = "CorrectWay Careers"
