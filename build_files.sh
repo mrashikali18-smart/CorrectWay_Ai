604f02fe7778b686ab0b2f7e4e580261f909f8ad
Mr.Ashik Ali <mr.ashikali18@gmail.com>
2026-08-02 15:47:17 +0530

    Update pip install command to allow breaking packages

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-python3 -m pip install --disable-pip-version-check -r requirements.txt
+python3 -m pip install --disable-pip-version-check --break-system-packages -r requirements.txt
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
